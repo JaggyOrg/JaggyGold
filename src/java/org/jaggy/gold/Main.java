8f2d5524732a53bfd842b678eaeaffd21ff03e22
Quirkylee <servingcities@gmail.com>
2018-07-04 01:27:01 -0400

    Setup the base of the plugin

---
+++ src/java/org/jaggy/gold/Main.java
@@ -0,0 +1,19 @@
+package org.jaggy.gold;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    /**
+     * Load stuff we need done before we enable
+     */
+    public void onLoad() {
+
+    }
+
+    /**
+     * Start up the plugin
+     */
+    public void onEnable() {
+
+    }
+}
